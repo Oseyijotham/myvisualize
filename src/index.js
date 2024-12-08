59f5809f0292870f27a25227bcef7f8ea08d691c
Oseyijotham <jothamoboh16@gmail.com>
2024-12-08 23:12:30 +0100

    Update App

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from 'components/App/App';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter basename="myvisualize">
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root') // Pass the root element here as the second argument
+);
---
+++ src/redux/store.jsx
@@ -0,0 +1,38 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { appReducer } from './Application/slice';
+import { authReducer } from './Auth/slice';
+//import { combineReducers } from 'redux';
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    app: appReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
